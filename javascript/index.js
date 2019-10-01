33107b846dca1f7086c1ab70a76c0d4b74ecaa81
mahmoudsh47@gmail.com <alaashaher12@gmail.com>
2019-10-01 20:26:05 +0200

    payment method

--- javascript/index.js
+++ javascript/index.js
@@ -15,29 +15,17 @@ var main = function () {
         if (firstName === "") {
             document.getElementById("#first").style.backgroundColor = "blue";
         }
-
-        return false;
-    });
-
-    $('form').submit(function () {
         var lastName = $('#last').val();
 
         if (lastName === "") {
             document.getElementById("#last").style.backgroundColor = "blue";
-        }
-
-        return false;
-    });
-
-    $('form').submit(function () {
+        } 
         var email = $('#email').val();
 
         if (email === "") {
             document.getElementById("#email").style.backgroundColor = "blue";
         };
-
-        return false;
-    })
+    });
 
 }
 
