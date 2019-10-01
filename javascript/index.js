4a1a38172cb8539469b2509a54fdf6173075e4dd
mahmoudsh47@gmail.com <alaashaher12@gmail.com>
2019-10-01 22:14:58 +0200

    cart details

--- javascript/index.js
+++ javascript/index.js
@@ -8,26 +8,51 @@ function closeNav() {
 
 // validation form
 
-var main = function () {
-    $('form').submit(function () {
+$(document).ready(function () {
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
         var firstName = $('#first').val();
 
         if (firstName === "") {
-            document.getElementById("#first").style.backgroundColor = "blue";
+            document.getElementById("first").style.backgroundColor = "bisque";
         }
         var lastName = $('#last').val();
 
         if (lastName === "") {
-            document.getElementById("#last").style.backgroundColor = "blue";
-        } 
+            document.getElementById("last").style.backgroundColor = "bisque";
+        }
         var email = $('#email').val();
 
         if (email === "") {
-            document.getElementById("#email").style.backgroundColor = "blue";
+            document.getElementById("email").style.backgroundColor = "bisque";
         };
+
+        var address = $('#address').val();
+        if (address === "") {            
+            document.getElementById("address").style.backgroundColor = "bisque";
+        };
+
+        var city = $('#city').val();
+        if (city === "") {
+            document.getElementById("city").style.backgroundColor = "bisque";
+        };
+
+        var zip = $('#zip').val();
+        if (zip === "") {
+            document.getElementById("zip").style.backgroundColor = "bisque";
+        } 
+        if (zip > 99999) {
+            document.getElementById("zip").style.backgroundColor = "bisque";
+        };
+
+        var phonenumber = $('#phonenumber').val();
+        if (phonenumber === "") {
+            document.getElementById("phonenumber").style.backgroundColor = "bisque";
+        };
+             
+        // if ($("#termss").prop('checked', false)) {            
+        //     document.getElementById("termss").style.backgroundColor = "bisque";
+        // };
     });
 
-}
-
-$(document).ready(main);
-
+});
